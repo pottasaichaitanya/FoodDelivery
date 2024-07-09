f437c97e34dc82d278994de5088d13a114f34275
Potta Sai Chaitanya <106024511+pottasaichaitanya@users.noreply.github.com>
2024-07-09 23:57:50 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import Orders from './pages/Orders/Orders'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 const App = () => {
-  const url='http://localhost:4000'
+  const url='https://fooddelivery-backend-z6s2.onrender.com'
   return (
     <div>
       <ToastContainer/>
