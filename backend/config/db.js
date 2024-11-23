793e2c75fe265bbca1823330b5b436f3755aafee
Potta Sai Chaitanya <106024511+pottasaichaitanya@users.noreply.github.com>
2024-11-24 01:09:55 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 export const connectDB=async()=>{
-    (await mongoose.connect('mongodb+srv://saichaitanyapotta21:12345@cluster0.ioistee.mongodb.net/food-del')
+    (await mongoose.connect('mongodb+srv://saichaitanyapotta21:12345@cluster0.wwttr.mongodb.net/food-del')
     .then(()=>console.log('db is connceted')
     ))
 }
